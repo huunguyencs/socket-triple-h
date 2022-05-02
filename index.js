851d324ad99074bdb633e2a8f8e9ba77f24115bf
Huu Nguyen <thieuvan0405@gmail.com>
2022-05-02 15:51:09 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const SocketServer = require('./socket');
+
+// middleware
+const app = express();
+
+//Socket
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+  pingTimeout: 60000,
+  cors: {
+    origin: 'https://triple-h.herokuapp.com'
+  }
+});
+
+io.on('connection', socket => {
+  SocketServer(socket);
+});
+
+// Port
+const PORT = process.env.PORT;
+
+http.listen(PORT, () => {
+  console.log('Server is running on port ', PORT);
+});
